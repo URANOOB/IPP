9350e92604865a5bc9418e9e69eb54eff387a1ab
URANOOB <will21@hotmail.es>
2026-04-24 18:42:24 -0500

    analytics vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Ingles pa' la Paz",
@@ -23,7 +24,10 @@ html {
 }
         `}</style>
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
